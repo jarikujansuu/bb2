c7f0347deb6dd7d1a564e63e9c8ac3639453d96c
jarikujansuu <jari.kujansuu@solinor.com>
2018-04-01 01:31:22 +0300

    BB2 API

---
+++ bb2api.py
@@ -0,0 +1,111 @@
+import requests
+
+
+pc = 'pc'
+ps4 = 'ps4'
+xbox1 = 'xb1'
+
+
+class Client(object):
+    apikey = None
+
+    def __init__(self, apikey):
+        self.apikey = apikey
+
+    def fetch(self, path, params=None):
+        if not params:
+            params = {}
+        params['key'] = self.apikey
+        return requests.get(f'http://web.cyanide-studio.com/ws/bb2{path}/', params).json()
+
+    def coaches(self, league=None, competition=None, platform=None, limit=None):
+        return self.fetch(
+            path='/coaches',
+            params={
+                'league': league,
+                'competition': competition,
+                'platform': platform,
+                'limit': limit
+            }
+        )
+
+    def teams(self, league=None, competition=None,platform=None,limit=None):
+        return self.fetch(
+            path='teams',
+            params={
+                'league': league,
+                'competition': competition,
+                'plaform': platform,
+                'limit': limit
+            }
+        )
+
+    def team(self, name=None, id=None):
+        return self.fetch(
+            path='/team',
+            params={
+                'name': name,
+                'id': id
+            }
+        )
+
+    def matches(self, league=None, competition=None, platform=None, limit=None, start=None, end=None, version=2):
+        return self.fetch(
+            path='/matches',
+            params={
+                'league': league,
+                'competition': competition,
+                'platform': platform,
+                'limit': limit,
+                'start': start,
+                'end': end,
+                'bb': version
+            })
+
+    def match(self, id, platform=None,version=2):
+        return self.fetch(
+            path='/match',
+            params={
+                'match_id': id,
+                'platform': platform,
+                'bb': version
+            }
+        )
+
+    def hall_of_fame(self, league=None, competition=None, platform=None,limit=None,exact_league_name=True):
+        return self.fetch(
+            path='/halloffame',
+            params={
+                'league': league,
+                'competition': competition,
+                'platform': platform,
+                'limit': limit,
+                'exact': int(exact_league_name)
+            }
+        )
+
+    def contests(self, league=None, competition=None, status=None, round=None, platform=None,limit=None,exact_league_name=True):
+        return self.fetch(
+            path='/contests',
+            params={
+                'league': league,
+                'competition': competition,
+                'status': status,
+                'round': int(round) if round else None,
+                'platform': platform,
+                'limit': limit,
+                'exact': int(exact_league_name) if exact_league_name else None
+            }
+        )
+
+    def ladder(self, league=None, competition=None, platform=None, ladder_size=None):
+        return self.fetch(
+            path='/ladder',
+            params={
+                'league': league,
+                'competition': competition,
+                'platform': platform,
+                'ladder_size': ladder_size
+            }
+        )
+
